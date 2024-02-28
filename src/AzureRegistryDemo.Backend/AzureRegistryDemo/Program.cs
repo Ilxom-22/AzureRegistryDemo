90f0763d7105dc18d1e676d2ee3711fdaee1c8c7
Ilhom Karimjonov <ilxomkarimjonov22@gmail.com>
2024-02-28 12:41:53 +0500

    Update db connection strings configuration

--- src/AzureRegistryDemo.Backend/AzureRegistryDemo/Program.cs
+++ src/AzureRegistryDemo.Backend/AzureRegistryDemo/Program.cs
@@ -1,6 +1,7 @@
 using AzureRegistryDemo.DataContexts;
 using AzureRegistryDemo.Extensions;
 using AzureRegistryDemo.SeedData;
+using AzureRegistryDemo.Settings;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,9 +10,11 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
+var dbConnectionString = builder.Configuration.GetSection(nameof(ConnectionStrings)).Get<ConnectionStrings>()?.DbConnectionString;
+
 builder.Services
     .AddDbContext<AppDbContext>(options =>
-        options.UseNpgsql(builder.Configuration.GetConnectionString("DbConnectionString")));
+        options.UseNpgsql(dbConnectionString));
         //options.UseInMemoryDatabase("AzureDemoRegistry"));
 
 var app = builder.Build();
